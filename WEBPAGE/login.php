dc6cc68415579a9231be2647437ea31aaaa5de69
raizon001 <118824873+raizon001@users.noreply.github.com>
2024-02-22 10:06:31 +0800

    Update login.php

--- WEBPAGE/login.php
+++ WEBPAGE/login.php
@@ -1,50 +1,33 @@
 <?php
-session_start();
-
 $host = "localhost";
 $user = "root";
 $password = "";
 $db = "user";
 
 $data = mysqli_connect($host, $user, $password, $db);
-$hashedPassword = password_hash('1234', PASSWORD_DEFAULT);
-echo $hashedPassword;
-
 
 if ($data === false) {
     die("Connection error");
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = mysqli_real_escape_string($data, $_POST["username"]);
-    $password = mysqli_real_escape_string($data, $_POST["password"]);
+// Hash the password before storing it in the database
+$hashedPassword = password_hash('1234', PASSWORD_DEFAULT);
 
-    $sql = "SELECT * FROM login WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($data, $sql);
+// Example registration query
+$username = "admin";
+$sql = "INSERT INTO login (username, password, usertype) VALUES ('$username', '$hashedPassword', 'admin')";
+$result = mysqli_query($data, $sql);
 
-    if ($result) {
-        $row = mysqli_fetch_array($result);
-
-        if ($row) {
-            $_SESSION["username"] = $username;
-
-            if ($row["usertype"] == "user") {
-                header("location: userhomepage.php");
-                exit();
-            } elseif ($row["usertype"] == "admin") {
-                header("location: admin.php");
-                exit();
-            } else {
-                echo "Invalid usertype";
-            }
-        } else {
-            echo "Username or password incorrect";
-        }
-    } else {
-        echo "Query execution error: " . mysqli_error($data);
-    }
+if ($result === false) {
+    echo "Registration failed: " . mysqli_error($data);
+} else {
+    echo "Registration successful!";
 }
+
+mysqli_close($data);
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -52,45 +35,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylelog.css" />
+    <link rel="stylesheet" href="loginstyle.css" />  
     <title>Login Form</title>
 </head>
 <body>
     <main>
-        <header>
-            <img src="lag.png" alt="logo">
-        </header>
-        <main>
-            <div class="Form-box">
-                <form class="Login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                    <h1>Admin Login</h1>
-                    <div class="input-box">
-                        <input type="text" name="username" required>
-                        <label>User:</label>
-                        <ion-icon name="mail-outline"></ion-icon>
-                    </div>
-                    <div class="input-box">
-                        <input type="password" name="password" required>
-                        <label>Password:</label>
-                        <ion-icon name="lock-closed-outline"></ion-icon>
-                    </div>
-                    <div class="checkbox">
-                        <span>
-                            <input type="checkbox" id="login-checkbox">
-                            <label for="login-checkbox">Remember Me</label>
-                        </span>
-                        <h5>Forget password ?</h5>
-                    </div>
-                    <button type="submit" class="submit-btn">Submit</button>
-                    <div class="goback-buttons">
-                        <a href="index.html" class="goback-button">Go Back &#x2190</a></h5>
-                    </div>
-                </form>
+    <header>
+
+            <img src="lag.png" alt="logo"> 
+    </header>
+    
+    <div class="Form-box">
+        <form class="Login-form">
+            <h1> Admin Login</h1>
+            <div class="input-box">
+                <input type="text" required>
+                <label>Username:</label>
+                <ion-icon name="mail-outline"></ion-icon>
             </div>
-            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-            <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-            <script src="script.js"></script>
-        </main>
-    </main>
+            <div class="input-box">
+                <input type="password" required>
+                <label>Password:</label>
+                <ion-icon name="lock-closed-outline"></ion-icon>
+            </div>
+            <div class="checkbox">
+                <span>
+                    <input type="checkbox" id="login-checkbox">
+                    <label for="login-checkbox">Remember Me</label>
+                </span>
+                <h5>Forget password ?</h5>
+            </div>
+
+            <div class="submitt-buttons"> 
+                <a href="admin.php" class="submit-button"> Submit</a></h5>
+             </div>
+            <div class="goback-buttons"> 
+               <a href="index.php" class="goback-button">Go Back &#x2190</a></h5>
+            </div>
+            </h5>
+        </form>
+        
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="script.js"></script>
+</main>
 </body>
 </html>
