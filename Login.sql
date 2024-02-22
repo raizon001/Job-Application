951cfa86ff97ef81dbfaceda2b52e0f4910ae7fa
raizon001 <118824873+raizon001@users.noreply.github.com>
2024-02-22 09:46:17 +0800

    Update Login.sql

--- Login.sql
+++ Login.sql
@@ -1,5 +1,6 @@
-CREATE TABLE users (
+CREATE TABLE login (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    usertype ENUM('user', 'admin') NOT NULL DEFAULT 'user'
 );
