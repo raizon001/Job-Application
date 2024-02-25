6f0eeefe8faa122968415392dcc4a198fd0a0a1b
raizon001 <118824873+raizon001@users.noreply.github.com>
2024-02-25 08:30:20 +0800

    Create view.php

---
+++ WEBPAGE/view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view.css" />
+    <title>Form</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <img src="lag.png" alt="logo">
+        </header>
+
+        <div class="container">
+            <div class="apply box">
+                <h2>Applicant List and tracking</h2>
+                <div class="goback-buttons">
+                        <a href="admin.php" class="goback-button">&#x2190</a>
+                    </div>
+</body>
+</main>
