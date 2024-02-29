7664185206d4012f76c06b328f3cbd9acd5839d9
raizon001 <118824873+raizon001@users.noreply.github.com>
2024-02-29 10:07:50 +0800

    Create record.php

---
+++ WEBPAGE/record.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formstyle.css" />
+    <title>Recordings</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <h2>Recordings</h2>
+        </header>
+
+        <div class="container">
+            <div class="records box">
+                <p>Click the button below to view recorded data:</p>
+                <a href="display_records.php" class="submit-btn">View Records</a>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
